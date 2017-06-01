9465f8db7d4cda6e523f00c3533a372bcee9d2f0
Abhishek <abhishek@incaendo.com>
2017-06-01 18:08:47 +0530

    html

--- sapp/scripts/app.js
+++ sapp/scripts/app.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('mlg_student', [ 'ngAnimate', 'ngCookies', 'ngRoute', 'ui.bootstrap','ngStorage'])
+angular.module('mlg_student', [ 'ngAnimate', 'ngCookies', 'ngRoute', 'ui.bootstrap','ngStorage','ngSanitize'])
 .value('urlParams', {
 users : '/users',
 login: '/users/login',
